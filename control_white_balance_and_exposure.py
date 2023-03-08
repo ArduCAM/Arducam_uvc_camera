8858c36c1f8c34c8629ed906a8827f2c8a8fc9e1
edward-ardu <113565969+edward-ardu@users.noreply.github.com>
2023-03-08 14:22:38 +0800

    Add device num.

--- control_white_balance_and_exposure.py
+++ control_white_balance_and_exposure.py
@@ -15,6 +15,7 @@ white_balance_temperature_value=5000
 exposure_value = 500
 exposure_step=50
 white_balance_temperature_step=500
+device=0
 
 
 def sh(cmd):
@@ -22,7 +23,7 @@ def sh(cmd):
     subprocess.check_call(cmd, shell=True)
 
 # Open camera
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(device)
 
 # Set resolution
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
@@ -33,8 +34,8 @@ print(width,height)
 if width == WIDTH and height == HEIGHT:
     print("The width and height are set successfully.")
 
-sh("v4l2-ctl -c white_balance_automatic=0")
-sh("v4l2-ctl -c white_balance_temperature={}".format(white_balance_temperature_value))
+sh("v4l2-ctl -c white_balance_automatic=0 -d {}".format(device))
+sh("v4l2-ctl -c white_balance_temperature={} -d {}".format(white_balance_temperature_value, device))
 
 cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 1)
 cap.set(cv2.CAP_PROP_EXPOSURE, 500)
@@ -52,25 +53,24 @@ while(True):
             white_balance_temperature_value += white_balance_temperature_step
             # sh("v4l2-ctl -l")
         print(white_balance_temperature_value)
-        sh("v4l2-ctl -c white_balance_temperature={}".format(white_balance_temperature_value))
-        sh("v4l2-ctl -l | grep white_balance_temperature")
+        sh("v4l2-ctl -c white_balance_temperature={} -d {}".format(white_balance_temperature_value, device))
+        sh("v4l2-ctl -l -d {} | grep white_balance_temperature".format(device))
     elif key == ord('d'):
         if white_balance_temperature_value > 2800:
             white_balance_temperature_value -= white_balance_temperature_step
         print(white_balance_temperature_value)
-        sh("v4l2-ctl -c white_balance_temperature={}".format(white_balance_temperature_value))
-        sh("v4l2-ctl -l | grep white_balance_temperature")
+        sh("v4l2-ctl -c white_balance_temperature={} -d {}".format(white_balance_temperature_value, device))
+        sh("v4l2-ctl -l -d {} | grep white_balance_temperature".format(device))
     elif key == ord('z'):
         if exposure_value < 5000:
             exposure_value += exposure_step
         print(exposure_value)
-        sh("v4l2-ctl -c exposure_time_absolute={}".format(exposure_value))
-        sh("v4l2-ctl -l | grep exposure_time_absolute")
+        sh("v4l2-ctl -c exposure_time_absolute={} -d {}".format(exposure_value, device))
+        sh("v4l2-ctl -l -d {} | grep exposure_time_absolute".format(device))
     elif key == ord('c'):
         if exposure_value > 0:
             exposure_value -= exposure_step
         print(exposure_value)
-        sh("v4l2-ctl -c exposure_time_absolute={}".format(exposure_value))
-        sh("v4l2-ctl -l | grep exposure_time_absolute")
-
+        sh("v4l2-ctl -c exposure_time_absolute={} -d {}".format(exposure_value, device))
+        sh("v4l2-ctl -l -d {} | grep exposure_time_absolute".format(device))
 
